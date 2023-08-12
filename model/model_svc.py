baf43655ae6e5b8b21ef3dabd8fde2cbd3e9dcd8
Gabriel Santos <gab.tec97@gmail.com>
2023-08-12 08:30:36 -0300

    creates a ml model with 77% of accuracy

---
+++ model/model_svc.py
@@ -0,0 +1,38 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+SEED = 20
+np.random.seed(SEED)
+
+
+def test_function():
+    file = [item for item in os.listdir() if item.endswith('.csv')][0]
+
+    df = pd.read_csv(file)
+    y = df["2urvived"]
+    x = df.drop("2urvived", axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.27, stratify=y)
+
+    # Modelo KNN
+    model = KNeighborsClassifier(metric="euclidean")
+    model.fit(x_train, y_train)
+
+    baseline = np.ones_like(y_test)
+    accuracy_baseline = accuracy_score(y_test, baseline)*100
+
+    x_test = x_test.fillna(0)
+    contiguous_x = np.ascontiguousarray(x_test)
+    previsions = model.predict(contiguous_x)
+
+    model_accuracy = accuracy_score(y_test, previsions)*100
+    print(classification_report(y_test, previsions))
+
+    return {
+        "accuracy_baseline": accuracy_baseline,
+        "model_accuracy": model_accuracy
+        }
